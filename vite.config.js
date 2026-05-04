408118a080931191776acf801c5b91b536a8253b
zxy <2545547553@qq.com>
2026-05-04 15:15:57 +0800

    添加仓库名

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/WLNL-breathing-and-heartbeat/', // 必须以 / 开头和结尾
   plugins: [vue()],
 })
+
