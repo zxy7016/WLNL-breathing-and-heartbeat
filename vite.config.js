67fd46b15e5e8f18344a092d711bb6474d6d5b24
zxy <2545547553@qq.com>
2026-05-04 15:42:29 +0800

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/WLNL-breathing-and-heartbeat/', // 必须以 / 开头和结尾
   plugins: [vue()],
 })
 
